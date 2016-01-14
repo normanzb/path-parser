493be7e630e4e62ba51e949d078d813f5bce9684
Norman Xu <im@norm.im>
2016-01-14 15:56:14 +0000

    first commit

---
+++ src/Parser.js
@@ -0,0 +1,53 @@
+define(function () {
+    'use strict';
+
+    function init(path) {
+        this._path = path;
+        this._path = this._path.replace(/\\/g, '/');
+        this._path = this._path.replace(/\/+/g, '/');
+
+        this._dirs = this._path.split('/');
+        if (this._dirs[0] === '') {
+            this._basedOnRoot = true;
+            this._dirs.shift();
+        }
+        else {
+            this._basedOnRoot = false;
+        }
+        this._file = this._dirs.pop();
+        this._ext = '';
+        var segs = this._file.split('.');
+        if (segs.length > 1) {
+            this._ext = segs.pop();
+        }
+        this._base = segs.join('.');
+    }
+
+    function Ctor(path) {
+        init.call(this, path);
+    }
+
+    var p = Ctor.prototype;
+
+    p.dirs = function() {
+        return this._dirs;
+    };
+
+    p.base = function() {
+        return this._base;
+    };
+
+    p.file = function() {
+        return this._base + (this._ext != ''?'.' + this._ext: '');
+    };
+
+    p.concat = function(path) {
+        return new Ctor(this.toString() + '/' + path);
+    };
+
+    p.toString = function() {
+        return (this._basedOnRoot?'/':'') + this._dirs.join('/') + '/' + this.file();
+    };
+
+    return Ctor;
+});
